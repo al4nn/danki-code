e3da73a69102de54b0925e42601cf8054c4a78bb
Alan Akira Sato Alves <alan.sato96@outlook.com>
2022-07-01 16:29:38 -0300

    Mais sobre variáveis e tipos de variáveis

---
+++ dev-web-completo/aula-04/index.php
@@ -0,0 +1,16 @@
+<?php
+  /* String */
+  $nome = 'Alan';
+  /* Número ou Inteiro */
+  $idade = 26;
+  /* Boolean */
+  $bool = true;
+  /* Doubles */
+  $double = 10.09;
+
+  /* Agora vou printar na tela minhas 2 variáveis */
+  echo 'Meu nome é: <b>' . $nome . '</b>, e tenho <b>' . $idade . '</b> Anos! <br/>';
+  echo $bool;
+  echo '<br/>';
+  echo $double;
+?>
